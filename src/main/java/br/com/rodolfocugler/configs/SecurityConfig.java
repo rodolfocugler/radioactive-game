3df4de507a18529dd9ef1b95b66ae5f3072991da
Rodolfo Cugler <rodolfocugler@outlook.com>
2020-12-22 20:04:17 -0300

    Cors

--- src/main/java/br/com/rodolfocugler/configs/SecurityConfig.java
+++ src/main/java/br/com/rodolfocugler/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.rodolfocugler.configs;
 import br.com.rodolfocugler.filters.JWTAuthenticationFilter;
 import br.com.rodolfocugler.filters.JWTAuthorizationFilter;
 import br.com.rodolfocugler.services.AccountService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static br.com.rodolfocugler.configs.AuthenticationConfig.SIGN_UP_URL;
 
@@ -28,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-            .csrf().disable()
+            .cors().and().csrf().disable()
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
             .anyRequest().authenticated()
@@ -41,5 +45,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(accountService).passwordEncoder(bCryptPasswordEncoder);
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
   }
 }
