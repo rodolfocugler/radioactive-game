75d1a9a896c4afb28d68e0b798ca175098e6a674
Rodolfo Cugler <rodolfocugler@outlook.com>
2020-12-28 12:29:55 -0300

    Add cors headers

--- src/main/java/br/com/rodolfocugler/configs/SecurityConfig.java
+++ src/main/java/br/com/rodolfocugler/configs/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +18,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import static br.com.rodolfocugler.configs.AuthenticationConfig.HEADER_STRING;
 import static br.com.rodolfocugler.configs.AuthenticationConfig.SIGN_UP_URL;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final AccountService accountService;
@@ -34,9 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-            .cors()
-            .configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-            .and().csrf().disable()
+            .cors().and().csrf().disable()
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
             .anyRequest().authenticated()
@@ -49,5 +46,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(accountService).passwordEncoder(bCryptPasswordEncoder);
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+    configuration.addExposedHeader(HEADER_STRING);
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
   }
 }
