0e9b2493d667e4cdcafe33342ec2166258bd3b33
Rodolfo Cugler <rodolfocugler@outlook.com>
2020-12-31 13:50:16 -0300

    Increase token time

--- src/main/java/br/com/rodolfocugler/configs/AuthenticationConfig.java
+++ src/main/java/br/com/rodolfocugler/configs/AuthenticationConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class AuthenticationConfig {
 
   public static final String SECRET = "0a046f598da583324875d93ff23f3657d4e6c025336bd08dc02f609878149b4fb6560829da98f4c28e90423480798010d1888b46c5096b7428c24a5cc63f8308";
-  public static final long EXPIRATION_TIME = 3600000;
+  public static final long EXPIRATION_TIME = 14400000;
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
   public static final String SIGN_UP_URL = "/api/accounts";
