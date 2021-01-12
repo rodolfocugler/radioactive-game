67ed2eebee6d4fa7f03e997ba4aefe4d5681fbe3
Rodolfo Cugler <rodolfocugler@outlook.com>
2021-01-12 16:38:32 -0300

    Add transport webSocket

--- src/main/java/br/com/rodolfocugler/controllers/TransportController.java
+++ src/main/java/br/com/rodolfocugler/controllers/TransportController.java
@@ -1,21 +1,30 @@
 package br.com.rodolfocugler.controllers;
 
+import br.com.rodolfocugler.domains.Account;
 import br.com.rodolfocugler.domains.Transport;
 import br.com.rodolfocugler.exceptions.DataNotFoundException;
 import br.com.rodolfocugler.services.TransportService;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/transports")
 public class TransportController {
-  public TransportController(TransportService transportService) {
+  public TransportController(TransportService transportService,
+                             SimpMessagingTemplate simpMessagingTemplate) {
     this.transportService = transportService;
+    this.simpMessagingTemplate = simpMessagingTemplate;
   }
 
   private final TransportService transportService;
+  private final SimpMessagingTemplate simpMessagingTemplate;
 
   @GetMapping("/{id}")
   public Transport get(@PathVariable long id) throws DataNotFoundException {
@@ -34,6 +43,16 @@ public class TransportController {
 
   @PostMapping
   public Transport add(@RequestBody @Validated Transport transport) {
-    return transportService.add(transport);
+    transport.setTimestamp(LocalDate.now().atStartOfDay()
+            .toInstant(OffsetDateTime.now().getOffset()).toEpochMilli());
+    Transport transportSaved = transportService.add(transport);
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    Account logged = (Account) authentication.getPrincipal();
+
+    simpMessagingTemplate.convertAndSend("/topic/transport/" +
+            + logged.getAccountGroup().getId(), transport);
+
+    return transportSaved;
   }
 }
